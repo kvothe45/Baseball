349cc24552da7d3cde6e14543081db6b5c76e61e
Ralph Beard <ralph.e.beard.iv@outlook.com>
2017-01-15 18:55:35 -0500

    basic authentication works

---
+++ Baseball.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Baseball.Data.Sql;
+using Baseball.Web.Models;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace Baseball.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(LoginRequestModel model)
+        {
+            var security = new SqlSecurity(ConfigurationManager.ConnectionStrings["Baseball"].ConnectionString);
+            var person = security.Authenticate(model.userName, model.password);
+
+
+
+            return View();
+        }
+    }
+}
---
+++ Baseball.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Baseball.Web.MvcApplication" Language="C#" %>
